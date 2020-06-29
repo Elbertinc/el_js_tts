ea6a8b22657832271ae85d1775aa7f53e09fe759
Elbertinc <50424152+Elbertinc@users.noreply.github.com>
2020-06-29 16:52:13 +0300

    test.js
    test text to speech

---
+++ test.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=320, initial-scale=1">
+    <meta charset="utf-8">
+    <style>
+      body, html {
+        min-width: 100%;
+        min-height: 100%;
+        margin: 0;
+        padding: 0;
+        font: Arial 14px;
+      }
+    </style>
+    <script src="el.js"></script>
+  </head>
+  <body>
+    <button onclick='elspeak("уж")'></button> 
+  </body>
+</html>
